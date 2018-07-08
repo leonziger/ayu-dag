639988192b177ecb9b6d86fccff4149bbdf87925
Leonid Zychkov <buldog52@gmail.com>
2018-07-08 22:44:49 +0300

    Solved problem with .modal.callback visibility

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const $ = window.$ = window.jQuery = require('jquery');
 
 import './components/modal/modal';
+import './components/main-header/main-header';
 // import './components/callback/callback'
---
+++ src/components/main-header/main-header.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import vex from 'vex-js';
+// import callback from '../callback/callback';
+
+
+$('.main-header__contacts-button').click(function (e) {
+  e.preventDefault();
+
+  const modal = $(`[data-modal=callback]`);
+
+  if (!modal.length) {
+    return console.error('Modal is not exist!');
+  }
+
+  vex.open({
+    unsafeContent: modal.html(),
+    closeClassName: 'modal__close',
+    // afterOpen: function () {
+    //   callback.mask();
+    // }
+  });
+});
